8fba5eea7a79f68abf40bfabdf35dbd1ecca4515
Dark <abdullahmedia@gmail.com>
2025-04-14 15:31:23 +0200

    why are there AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:secure_crypt/core/page_view/page_view.dart';
 import 'package:secure_crypt/features/Home/home_screen.dart';
 import 'package:secure_crypt/core/splash.dart';
+import 'package:secure_crypt/utils/color_app.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: ColorApp.colorBg
+      ),
+      initialRoute: SplashScreen.routeName,
       routes: {
               SplashScreen.routeName: (_) => SplashScreen(),
               HomeScreen.routeName: (_) => HomeScreen(),
               MyPageView.routeName: (_) => MyPageView(),
       },
-      initialRoute: SplashScreen.routeName,
     );
   }
 }
